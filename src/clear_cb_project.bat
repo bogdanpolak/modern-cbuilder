dc859d46111fb58e463a2d243fea6609828b4558
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-02-26 14:42:44 +0100

    Added clean_project.bat

---
+++ src/clear_cb_project.bat
@@ -0,0 +1,20 @@
+del /Q __history\*.*
+rmdir __history
+
+del /S /Q Win32\*.*
+rmdir /S /Q Win32
+
+del /S /Q Debug\*.*
+rmdir /S /Q Debug
+
+del desktop.ini
+del *.identcache
+del *.cbproj.local
+del *.il?
+del *.map
+del *.obj
+del *.pch
+del *.tds
+del *.#*
+
+del *.exe
