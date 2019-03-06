275005c9eb354e0f77464d1e54827c3de899013c
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-03-06 09:49:23 +0100

    Added comments for std::atomic

--- demo/01-cpp17/DemoAtomics.cpp
+++ demo/01-cpp17/DemoAtomics.cpp
@@ -7,6 +7,11 @@
 #pragma package(smart_init)
 
 #include <iostream>
+#include <atomic>
 
 void demo_atomics () {
+    // atomics are not supported yet
+	struct Counters { int a; int b; };
+	// Counters is trivially-copyable type
+	// std::atomic<Counters> cnt;
 }
--- demo/01-cpp17/Main.cpp
+++ demo/01-cpp17/Main.cpp
@@ -42,14 +42,14 @@ void block_parallel_code() {
 	demo_std_thread_and_mutex();
 	std::cout << "[demo] from cppreference std::thread and std::mutex... \n";
 	demo_cppreference_mutex_thread();
-	std::cout << "[demo] atomics ... \n";
+	// std::cout << "[demo] atomics ... \n";
 	demo_atomics();
 }
 
 int _tmain(int argc, _TCHAR* argv[]) {
 	block_cpp17_basics();
-	// block_code_migration();
-	// block_parallel_code();
+	block_code_migration();
+	block_parallel_code();
 	// --------------------
 	std::system("pause");
 	return 0;
